8012ba8d4386357712ce6a65719bbcf9d5997528
AndyLu <andyludeveloper@gmail.com>
2020-06-17 18:37:05 +0800

    refactor: extract the same part

--- src/main/java/com/andyludeveloper/aut/LogAnalyzer.kt
+++ src/main/java/com/andyludeveloper/aut/LogAnalyzer.kt
@@ -3,8 +3,14 @@ package com.andyludeveloper.aut
 import java.lang.IllegalArgumentException
 
 class LogAnalyzer {
+    var wasLastFileNameValid:Boolean = false
     fun isValidLogFileName(filename:String?):Boolean{
+        wasLastFileNameValid = false
         filename?:throw IllegalArgumentException("filename has to be provided")
-        return filename.endsWith(".SLF", true)
+        if(!filename.endsWith(".SLF", true)){
+            return false
+        }
+        wasLastFileNameValid = true
+        return true
     }
 }
--- src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
+++ src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
@@ -1,12 +1,15 @@
 package com.andyludeveloper.aut
 
-import org.junit.Assert.assertTrue
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.*
 import org.junit.Test
 import java.lang.IllegalArgumentException
 import kotlin.test.assertFailsWith
 
 
 class LogAnalyzerTest{
+    private lateinit var SUT : LogAnalyzer
+
     @Test
     fun isValidLogFileName_GoodExtensionLowercase_ReturnTrue() {
         val SUT = LogAnalyzer()
@@ -18,7 +21,7 @@ class LogAnalyzerTest{
     fun isValidLogFileName_GoodExtensionUppercase_ReturnTrue() {
         val SUT = LogAnalyzer()
         val result = SUT.isValidLogFileName("filewithgoodextension.SLF")
-            assertTrue(result)
+        assertTrue(result)
     }
 
     @Test
@@ -27,5 +30,26 @@ class LogAnalyzerTest{
         assertFailsWith<IllegalArgumentException> {
             SUT.isValidLogFileName(null)
         }
+    }
+
+    @Test
+    fun isValidLogFileName_BadName_WasLastFileNameValid_Return_False() {
+        givenFileName("badname.foo")
+        shouldBe(false)
+    }
+
+    @Test
+    fun isValidLogFileName_WhenCalled_WasLastFileNameValid_Return_True() {
+        givenFileName("GoodName.SLF")
+        shouldBe(true)
+    }
+
+    private fun shouldBe(result:Boolean) {
+        assertThat(SUT.wasLastFileNameValid, `is`(result))
+    }
+
+    private fun givenFileName(filename: String) {
+        SUT = LogAnalyzer()
+        SUT.isValidLogFileName(filename)
     }
 }
