6993553a40e4b43a1b69a2597eedb6572ccb28a2
AndyLu <andyludeveloper@gmail.com>
2020-06-20 14:14:13 +0800

    refactor: Initial SUT in class parameter

--- src/test/java/com/andyludeveloper/aut/MemCalculatorTest.kt
+++ src/test/java/com/andyludeveloper/aut/MemCalculatorTest.kt
@@ -5,20 +5,25 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class MemCalculatorTest {
+    private val SUT = MemCalculator()
 
     @Test
     fun sum_ByDefault_Return0() {
-        val SUT = MemCalculator()
         val result = SUT.sum()
         assertEquals(0 ,result)
     }
     @Test
     fun add_WhenCalled_ChangesSum() {
-        val SUT = MemCalculator()
         SUT.add(1)
         val result = SUT.sum()
         assertEquals(1 ,result)
     }
 
+    @Test
+    fun add_Input2_Return2() {
+        SUT.add(2)
+        val result = SUT.sum()
+        assertEquals(2 ,result)
+    }
 
 }
