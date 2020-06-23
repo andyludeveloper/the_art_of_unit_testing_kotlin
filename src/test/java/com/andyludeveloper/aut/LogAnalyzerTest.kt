6a4933b869a5ef256e5d1836eae8bd1ed07e35f7
AndyLu <andyludeveloper@gmail.com>
2020-06-23 14:15:37 +0800

    Listing 3.5 Injecting a fake by adding property setters to the class under test

--- src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
+++ src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
@@ -4,11 +4,21 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class LogAnalyzerTest{
+
     @Test
     fun isValidFileName_NameSupportedExtension_ReturnsTrue(){
         val manager = FakeValidFakeExtensionManager()
         manager.valid = true
         val SUT = LogAnalyzer(manager)
+        val result =SUT.isValidLogFileName("short.ext")
+        assertTrue(result)
+    }
+    @Test
+    fun isValidFileName_NameSupportedExtension_add_property_ReturnsTrue(){
+        val manager = FakeValidFakeExtensionManager()
+        manager.valid = true
+        val SUT = LogAnalyzer()
+        SUT.manager = manager
         val result =SUT.isValidLogFileName("short.ext")
         assertTrue(result)
     }
