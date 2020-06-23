6f48f00392117b73fe321e3e40c5ad30b54464f2
AndyLu <andyludeveloper@gmail.com>
2020-06-23 13:55:32 +0800

    Init ch3 branch

--- src/main/java/com/andyludeveloper/aut/LogAnalyzer.kt
+++ src/main/java/com/andyludeveloper/aut/LogAnalyzer.kt
@@ -3,14 +3,11 @@ package com.andyludeveloper.aut
 import java.lang.IllegalArgumentException
 
 class LogAnalyzer {
-    var wasLastFileNameValid:Boolean = false
     fun isValidLogFileName(filename:String?):Boolean{
-        wasLastFileNameValid = false
         filename?:throw IllegalArgumentException("filename has to be provided")
         if(!filename.endsWith(".SLF", true)){
             return false
         }
-        wasLastFileNameValid = true
         return true
     }
 }
--- src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
+++ src/test/java/com/andyludeveloper/aut/LogAnalyzerTest.kt
@@ -8,48 +8,5 @@ import kotlin.test.assertFailsWith
 
 
 class LogAnalyzerTest{
-    private lateinit var SUT : LogAnalyzer
 
-    @Test
-    fun isValidLogFileName_GoodExtensionLowercase_ReturnTrue() {
-        val SUT = LogAnalyzer()
-        val result = SUT.isValidLogFileName("filewithgoodextension.slf")
-        assertTrue(result)
-    }
-
-    @Test
-    fun isValidLogFileName_GoodExtensionUppercase_ReturnTrue() {
-        val SUT = LogAnalyzer()
-        val result = SUT.isValidLogFileName("filewithgoodextension.SLF")
-        assertTrue(result)
-    }
-
-    @Test
-    fun isValidLogFileName_NullFileName_Throws() {
-        val SUT = LogAnalyzer()
-        assertFailsWith<IllegalArgumentException> {
-            SUT.isValidLogFileName(null)
-        }
-    }
-
-    @Test
-    fun isValidLogFileName_BadName_WasLastFileNameValid_Return_False() {
-        givenFileName("badname.foo")
-        shouldBe(false)
-    }
-
-    @Test
-    fun isValidLogFileName_WhenCalled_WasLastFileNameValid_Return_True() {
-        givenFileName("GoodName.SLF")
-        shouldBe(true)
-    }
-
-    private fun shouldBe(result:Boolean) {
-        assertThat(SUT.wasLastFileNameValid, `is`(result))
-    }
-
-    private fun givenFileName(filename: String) {
-        SUT = LogAnalyzer()
-        SUT.isValidLogFileName(filename)
-    }
 }
